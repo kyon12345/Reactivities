4df4be8776d2f028712e090df6ded67794d0d74a
a13576034204 <a13576034204@163.com>
2019-11-17 11:03:08 +0800

    add Edit CommandHandler,modify Create

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,13 +1,13 @@
-using Domain;
-using MediatR;
-using Persistence;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
+using Persistence;
 
 namespace Application.Activities
 {
-    public class Create
+
+    public class Edit
     {
         public class Command : IRequest
         {
@@ -15,7 +15,7 @@ namespace Application.Activities
             public string Title { get; set; }
             public string Description { get; set; }
             public string Category { get; set; }
-            public DateTime Date { get; set; }
+            public DateTime? Date { get; set; }
             public string City { get; set; }
             public string Venue { get; set; }
         }
@@ -25,26 +25,29 @@ namespace Application.Activities
             private readonly DataContext _context;
             public Handler(DataContext context)
             {
+                this._context = context;
                 _context = context;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = new Activity
-                {
-                    Id = request.Id,
-                    Title = request.Title,
-                    Description = request.Description,
-                    Category = request.Category,
-                    Date = request.Date,
-                    City = request.City,
-                    Venue = request.Venue
-                };
+                var activity = await _context.Activities.FindAsync(request.Id);
 
-                _context.Activities.Add(activity);
+                if(activity==null)
+                    throw new Exception("Could not find activity");
+
+                activity.Title = request.Title ?? activity.Title;
+                activity.Description = request.Description ?? activity.Description;
+                activity.Category = request.Category ?? activity.Category;
+                activity.Date = request.Date ?? activity.Date;
+                activity.City = request.City ?? activity.City;
+                activity.Venue = request.Venue ?? activity.Venue;
+                
+
+
                 var success = await _context.SaveChangesAsync() > 0;
 
-                if (success) return Unit.Value;
+                if(success) return Unit.Value;
 
                 throw new Exception("Problem saving changes");
             }
