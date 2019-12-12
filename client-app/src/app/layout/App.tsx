6fd045b42ff0b8123b50739d74f467b0373bd262
kyon12345 <a13576034204@163.com>
2019-12-12 16:52:26 +0800

    use react hooks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
-import { Header, Icon, List } from 'semantic-ui-react';
-import axios from 'axios';
-import {IAcitvity} from '../models/activity'
+import React, { useState, useEffect } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
+import { IAcitvity } from "../models/activity";
 
-interface IState{
-  activities:IAcitvity[]
+interface IState {
+  activities: IAcitvity[];
 }
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities:[]
-  }
+const App = () => {
+  const [activities, setActivities] = useState<IAcitvity[]>([]);
 
-  componentDidMount() {
-    axios.get<IAcitvity[]>('http://localhost:5000/api/activities').then(response => {
-      this.setState({
-        activities: response.data
+  useEffect(() => {
+    axios
+      .get<IAcitvity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
       });
-    });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
