da023ece41fccd64ac8467621ab24cb2fc66a574
kyon12345 <a13576034204@163.com>
2020-01-21 17:19:54 +0800

    add client-side error handling

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import {  Grid, GridColumn } from "semantic-ui-react";
+import { Grid, GridColumn } from "semantic-ui-react";
 import ActivityStore from "../../../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
 import { RouteComponentProps } from "react-router-dom";
@@ -13,20 +13,21 @@ interface DetailParams {
   id: string;
 }
 
-const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match }) => {
+const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history
+}) => {
   const activityStore = useContext(ActivityStore);
-  const {
-    activity,
-    loadActivity,
-    loadingInitial
-  } = activityStore;
+  const { activity, loadActivity, loadingInitial } = activityStore;
 
   useEffect(() => {
-    loadActivity(match.params.id)
-  }, [loadActivity, match.params.id])
+    loadActivity(match.params.id);
+  }, [loadActivity, match.params.id,history]);
 
-  if (loadingInitial || !activity)
+  if (loadingInitial)
     return <LoadingComponent content="loading activity ..." />;
+  
+  if (!activity) return <h2>Activity Not Found</h2>
 
   return (
     <Grid>
