cd65d4627a5468475c3ffeffa74635ec0b1d4845
kyon12345 <a13576034204@163.com>
2020-01-22 08:32:13 +0800

    remove error

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -7,26 +7,29 @@ using Domain;
 using MediatR;
 using Persistence;
 
-namespace Application.Activities {
-    public class Details {
-        public class Query : IRequest<Activity> {
+namespace Application.Activities
+{
+    public class Details
+    {
+        public class Query : IRequest<Activity>
+        {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity> {
+        public class Handler : IRequestHandler<Query, Activity>
+        {
             private readonly DataContext _context;
-            public Handler (DataContext context) {
+            public Handler(DataContext context)
+            {
                 this._context = context;
             }
 
-            public async Task<Activity> Handle (Query request, CancellationToken cancellationToken) {
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
 
-                throw new Exception ("test server error");
-
-                var activity = await _context.Activities.FindAsync (request.Id);
-
-                if (activity == null)
-                    throw new RestException (HttpStatusCode.NotFound, new { Activity = "Not Found" });
+                 if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { Activity = "Not Found" });
 
                 return activity;
             }
