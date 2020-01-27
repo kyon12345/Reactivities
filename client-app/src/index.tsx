1468846ef39cbf8e87584431b5538ae0b1ae3b05
kyon12345 <a13576034204@163.com>
2020-01-27 22:08:57 +0800

    add react-widgets

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import "react-toastify/dist/ReactToastify.min.css";
+import "react-widgets/dist/css/react-widgets.css";
 import ScrollToTop from './app/layout/ScrollToTop';
+import dateFnsLocalizer from "react-widgets-date-fns";
 
 export const history = createBrowserHistory();
 
