8021dd7bcc1def26214a56d6a95feb5caf2e7935
kyon12345 <a13576034204@163.com>
2020-03-03 09:58:04 +0800

    add Login Controller

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -12,7 +12,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -27,7 +27,7 @@ namespace Application.User
             }
         }
 
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly DataContext _context;
             public UserManager<AppUser> _userManager { get; }
@@ -39,7 +39,7 @@ namespace Application.User
                 _context = context;
             }
 
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
                 if(user==null){
@@ -50,7 +50,13 @@ namespace Application.User
                 .CheckPasswordSignInAsync(user, request.Password, false);
 
                 if(result.Succeeded){
-                    return user;
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "this is a token",
+                        Username = user.UserName,
+                        Image = null
+                    };
                 }
 
                 throw new RestException(HttpStatusCode.Unauthorized);
