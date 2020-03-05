96741d9d58ef9af5362d0e8f5b28a2050296699d
kyon12345 <a13576034204@163.com>
2020-03-05 15:37:06 +0800

    fixed erors in LoginForm

--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,60 +1,74 @@
 import React, { useContext } from "react";
-import { Form as FinalForm, Field, useFormState } from "react-final-form";
+import { Form as FinalForm, Field } from "react-final-form";
 import { IUserFormValues } from "../../app/models/user";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import { FORM_ERROR } from "final-form";
 import { combineValidators, isRequired } from "revalidate";
-import { Form, Button, Header } from "semantic-ui-react";
+import { Form, Button, Header, Label } from "semantic-ui-react";
 import TextInput from "../../app/common/form/TextInput";
 
 const validate = combineValidators({
-  email: isRequired("Email"),
-  password: isRequired("Password")
+    email: isRequired("Email"),
+    password: isRequired("Password")
 });
 
 const LoginForm = () => {
-  const rootStore = useContext(RootStoreContext);
-  const { login } = rootStore.userStore;
-  return (
-    <FinalForm
-      onSubmit={(values: IUserFormValues) =>
-        login(values).catch(error => ({ [FORM_ERROR]: error }))}
-      // onSubmit={(values)=>console.log(values)}
-      validate={validate}
-      render={({
-        handleSubmit,
-        submitting,
-        submitError,
-        invalid,
-        pristine,
-        dirtySinceLastSubmit
-      }) => (
-        <Form onSubmit={handleSubmit} error>
-          <Header
-            as="h2"
-            content="Login to Reactivities"
-            color="teal"
-            textAlign="center"
-          />
-          <Field name="email" component={TextInput} placeholder="Email" />
-          <Field
-            name="password"
-            type="password"
-            component={TextInput}
-            placeholder="Password"
-          />
-          {submitError && !dirtySinceLastSubmit && { submitError }}
-          <Button
-            disabled={(invalid && !dirtySinceLastSubmit) || pristine}
-            loading={submitting}
-            color="teal"
-            content="Login"
-            fluid
-          />
-        </Form>
-      )}
-    />
-  );
+    const rootStore = useContext(RootStoreContext);
+    const { login } = rootStore.userStore;
+    return (
+        <FinalForm
+            onSubmit={(values: IUserFormValues) =>
+                login(values).catch(error => ({ [FORM_ERROR]: error }))
+            }
+            validate={validate}
+            render={({
+                handleSubmit,
+                submitting,
+                submitError,
+                invalid,
+                pristine,
+                dirtySinceLastSubmit,
+                form
+            }) => (
+                <Form onSubmit={handleSubmit} error>
+                    <Header
+                        as="h2"
+                        content="Login to Reactivities"
+                        color="teal"
+                        textAlign="center"
+                    />
+                    <Field
+                        name="email"
+                        component={TextInput}
+                        placeholder="Email"
+                    />
+                    <Field
+                        name="password"
+                        type="password"
+                        component={TextInput}
+                        placeholder="Password"
+                    />
+                    {submitError && !dirtySinceLastSubmit && (
+                        <Label
+                            color="red"
+                            basic
+                            content={submitError.statusText}
+                        />
+                    )}
+                    <Button
+                        disabled={
+                            (invalid && !dirtySinceLastSubmit) || pristine
+                        }
+                        loading={submitting}
+                        color="teal"
+                        content="Login"
+                        fluid
+                    />
+                    <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
+                </Form>
+            )}
+        />
+    );
 };
 
 export default LoginForm;
