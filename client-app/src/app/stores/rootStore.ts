3fc05e9785c00974792dfab69374f527ff48e23d
kyon12345 <a13576034204@163.com>
2020-03-16 14:49:30 +0800

    get profile data and display user images

--- client-app/src/app/stores/rootStore.ts
+++ client-app/src/app/stores/rootStore.ts
@@ -4,6 +4,7 @@ import { createContext } from "react";
 import { configure } from "mobx";
 import CommonStore from "./commonStore";
 import ModalStore from "./modalStore";
+import ProfileStroe from "./profileStore";
 
 configure({ enforceActions: "always" });
 
@@ -12,12 +13,14 @@ export default class RootStore {
     userStore: UserStore;
     commonStore: CommonStore;
     modalStore: ModalStore;
+    profileStore: ProfileStroe;
 
     constructor() {
         this.activityStore = new ActivityStore(this);
         this.userStore = new UserStore(this);
         this.commonStore = new CommonStore(this);
         this.modalStore = new ModalStore(this);
+        this.profileStore = new ProfileStroe(this);
     }
 }
 
