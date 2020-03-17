cea7a868e57c00c8b2d722ee50174bd7e48e26b8
kyon12345 <a13576034204@163.com>
2020-03-17 11:15:27 +0800

    Add PhotoWidget Cropper

--- client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
@@ -2,9 +2,11 @@ import React, { Fragment, useState, useEffect } from "react";
 import { Header, Grid, Image } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
 export const PhotoUploadWidget = () => {
     const [files, setFiles] = useState<any[]>([]);
+    const [image, setImage] = useState<Blob | null>(null);
 
     useEffect(() => {
         return () => {
@@ -22,6 +24,12 @@ export const PhotoUploadWidget = () => {
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
                     <Header sub color="teal" content="Step 2 - Resize image" />
+                    {files.length > 0 && (
+                        <PhotoWidgetCropper
+                            setImage={setImage}
+                            imagePreview={files[0].preview}
+                        />
+                    )}
                 </Grid.Column>
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
@@ -29,8 +37,11 @@ export const PhotoUploadWidget = () => {
                         sub
                         color="teal"
                         content="Step 3 - Preview & Upload"
-            />
-            {files.length > 0 && (<Image src={files[0].preview} />)}
+                    />
+                    <div
+                        className="img-preview"
+                        style={{ minHeight: "200px", overflow: "hidden" }}
+                    />
                 </Grid.Column>
             </Grid>
         </Fragment>
